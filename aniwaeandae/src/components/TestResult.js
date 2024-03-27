68860fe5e772d67bd79c5dcff201162284dde1a6
Yerim <yearm404@naver.com>
2024-03-27 20:06:59 +0900

    Explain of quiz comeout but broken

--- aniwaeandae/src/components/TestResult.js
+++ aniwaeandae/src/components/TestResult.js
@@ -1,18 +1,39 @@
-// TestResult.js
-import React from 'react';
-import styles from './TestResult.module.css'; // CSS 모듈 임포트
+import React, { useState } from 'react';
+import styles from './TestResult.module.css';
 
 const TestResult = ({ testResult }) => {
+ // 각 문제에 대한 해설 상태를 관리하는 배열을 초기화합니다.
+ const [showExplanations, setShowExplanations] = useState(new Array(testResult.length).fill(false));
+
+ const handleMouseOver = (index) => {
+    setShowExplanations(prev => {
+      const newShowExplanations = [...prev];
+      newShowExplanations[index] = true;
+      return newShowExplanations;
+    });
+ };
+
+ const handleMouseOut = (index) => {
+    setShowExplanations(prev => {
+      const newShowExplanations = [...prev];
+      newShowExplanations[index] = false;
+      return newShowExplanations;
+    });
+ };
+
  return (
-    <div className={styles.testResultContainer}>
+    <div>
       {testResult ? (
         <div>
           {testResult.map((item, index) => (
-            <div key={index}>
-              <p>{index+1}. {item.question}
-              <p>내가 고른 답 :{item.answer} 
-              {item.isCorrect ? 'O' : 'X'}
-              <p>{item.explain}</p></p></p>
+            <div
+              key={index}
+              onMouseOver={() => handleMouseOver(index)}
+              onMouseOut={() => handleMouseOut(index)}
+            >
+              <p>{index+1}. {item.question}</p>
+              <p>내가 고른 답 :{item.answer} {item.isCorrect ? 'O' : 'X'}</p>
+              <div className='explain'>{showExplanations[index] && <p>{item.explain}</p>}</div>
             </div>
           ))}
         </div>
